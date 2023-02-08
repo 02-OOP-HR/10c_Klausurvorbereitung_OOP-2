4c20812a01d335c5866e8018c2eacb66da579c0d
HROO198 <heinrich.roos@o2online.de>
2023-02-08 10:52:26 +0100

    Fertig

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -8,12 +8,24 @@ public class App {
      Test test = new Test("Grizabella", 29);
 
      System.out.println(test.getName());
+       
+     // Eigener TEST
 
-
-
-
-
-    
+    //  StringBuilder abc = new StringBuilder(5);
+    //  System.out.println("Stringbuilder: " + abc);
+    //  System.out.println("Length: " + abc.length());
+    //  System.out.println("Capacity: " + abc.capacity());
+    // abc.append("X");
+    // System.out.println("Length: " + abc.length());
+    // System.out.println("Capacity: " + abc.capacity());
+    // abc.append("YZAB");
+    // System.out.println("Length: " + abc.length());
+    // System.out.println("Capacity: " + abc.capacity());
+    // System.out.println(abc);
+    // abc.setLength(15);
+    // System.out.println("Length: " + abc.length());
+    // System.out.println("Capacity: " + abc.capacity());
+    // System.out.println(abc);
     }
 
 }
